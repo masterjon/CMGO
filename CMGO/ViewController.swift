507e451cc37cb8cdb3d187fb0d3f0463924628d5
Jonathan Horta <jonabc7@gmail.com>
2019-08-19 22:54:34 -0500

    Initial Commit

---
+++ CMGO/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CMGO
+//
+//  Created by Jonathan Horta on 8/19/19.
+//  Copyright © 2019 iddeas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
